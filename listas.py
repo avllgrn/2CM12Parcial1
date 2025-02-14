254a4f6fbe7649a5548309609cc4eb4cd0aa2cd3
Alfredo Villagran Olguin <avillagran@ipn.mx>
2025-02-14 08:55:19 -0600

    Suma acumulada de [0, n)

--- listas.py
+++ listas.py
@@ -1,22 +1,12 @@
 from os import system
-from random import randrange
-
-def funcionConRandrange():
-    V = []
-    n = int(input('Cuantos datos? '))
-
-    for i in range(n):
-        dato = randrange(100)
-        print(dato, type(dato))
-        V.append(dato)
-    print(V)
-
-    posicion = int(input('Cual quieres cambiar? '))
-    V[posicion] = -123
-    print(V)
-
 
 if __name__ == '__main__':
     system('cls')
-    funcionConRandrange()
-    print()
+    
+    n = int(input('Cuantos? '))
+    s = 0
+    
+    for i in range(n):
+        s = s+i
+        print(f'i={i}\ts={s}')
+        
