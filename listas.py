3d7a4ef527d84ef7653519ec8e80963b12c7324a
avllgrn <avillagran@ipn.mx>
2025-03-10 11:21:12 -0600

    Menu para Pila (suponiendo que existe archivo Pilas.py con la clase)

--- listas.py
+++ listas.py
@@ -1,69 +1,39 @@
+import Pilas
 from os import system
 
-class Nodo:
-    def __init__(self, dato=None, siguiente=None):
-        self.dato = dato
-        self.siguiente = siguiente
+def menu():
+    print('1. Push')
+    print('2. Pop')
+    print('3. Libera')
+    print('4. Salir')
+    return input('Cuál es tu opción? ')    
 
-    def __str__(self):
-        cadena = '| '
-        cadena += str(self.dato) + ' |'
-        if self.siguiente != None:
-            cadena += ' -> '
-        return cadena
-
-class Cola:
-    def __init__(self):
-        self.primero = None
-        self.ultimo = None
-
-    def __del__(self):
-        self.liberaMemoria()
-
-    def liberaMemoria(self):
-        while not self.estaVacia():
-            print(self.pop())
-
-    def estaVacia(self):
-        return self.primero == None and self.ultimo == None
-
-    def push(self, dato):
-        if self.estaVacia():
-            self.ultimo = Nodo(dato, None)
-            self.primero = self.ultimo
-        else:
-            self.ultimo.siguiente = Nodo(dato, None)
-            self.ultimo = self.ultimo.siguiente
-
-    def pop(self):
-        d = None
-        if not self.estaVacia():
-            d = self.primero.dato 
-            if self.primero != self.ultimo:
-                aux = self.primero
-                self.primero = self.primero.siguiente
-                del aux
+def casos(opcion, P):
+    match opcion:
+        case '1':
+            x = input('Teclea dato a insertar ')
+            P.push(x)
+        case '2':
+            if not P.estaVacia():
+                print(f'Sale {P.pop()}')
             else:
-                del self.primero
-                self.primero = None
-                self.ultimo = None
-        return d
+                print('La pila está vacía...')
+            system('pause')
+        case '3':
+            P.liberaMemoria()
+            system('pause')
+        case '4':
+            print('Adiós!')
+        case _:
+            print('Opción inválida!')
+            system('pause')
 
 if __name__ == '__main__':
     system('cls')
-
-    C = Cola()
-
-    C.push(5.1)
-    C.push(3.1)
-    C.push(7.1)
-    C.push(-4.1)
-
-    print(C.pop(), ' -> ', end='')
-    print(C.pop(), ' -> ', end='')
+    P = Pilas.Pila()
     
-    C.push(9.1)
-
-    print(C.pop(), ' -> ', end='')
-    print(C.pop(), ' -> ', end='')
-    print(C.pop(), ' -> ', end='')
+    opcion = None
+    while opcion!='4':
+        system('cls')
+        opcion = menu()
+        casos(opcion, P)
